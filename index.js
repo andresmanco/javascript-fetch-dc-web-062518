a09f45f4ce230c26193967010d213c59d831d4c0
github username <github email address>
2018-08-10 12:22:41 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-const app = "I don't do much.";
+const token =  'dc4067c79e007a5a8ba50e3533dafa81ceed1232'
+fetch('https://api.github.com/repos/jquery/jquery/commits', {
+  headers: {
+    Authorization: `token ${token}`
+  }
+}).then(res=> res.json()).then(json=> console.log(json));
